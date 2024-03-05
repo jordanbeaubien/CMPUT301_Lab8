164fc3ea083287feab1bf959e426037c80d064fb
jordanbeaubien <jordanbeaubien@me.com>
2024-03-05 09:40:38 -0700

    push test for lab8

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -12,5 +12,24 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    /**
+     * create a mocklist for my citylist * @return
+     */
+    public CustomList MockCityList(){
+        return new CustomList(null,new ArrayList<>());
+    }
+
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size plus one
+     */
+    @Test
+    public void addCityTest(){
+        CustomList cityList = MockCityList();
+        int listSize = cityList.getCount();
+        cityList.addCity(new City("Estevan", "SK"));
+        assertEquals(cityList.getCount(),listSize + 1);
+    }
 
 }
